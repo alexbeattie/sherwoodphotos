a9aebb20900a0c55c37b7bdb88e22dca01a30ae3
Alex Beattie <alexbeattie@Alexs-MacBook-Pro.local>
2018-05-24 11:02:47 -0700

    added constants and into view controller with loop of photos

---
+++ sherwoodrealestate/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  sherwoodrealestate
+//
+//  Created by Alex Beattie on 5/23/18.
+//  Copyright © 2018 Alex Beattie. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://http://sparkapi.com/v1/"
+let GET_URL = "http://http://sparkapi.com/v1"
+let MY_LISTINGS_PASS = "uTqE_dbyYSx6R1LvonsWOApiKeyvc_c15909466_key_1ServicePath/v1/my/listingsAuthToken"
+let ALL_LISTINGS = "uTqE_dbyYSx6R1LvonsWOApiKeyvc_c15909466_key_1ServicePath/v1/listingsAuthToken"
+let MY_LISTINGS_PASS_SORT = "uTqE_dbyYSx6R1LvonsWOApiKeyvc_c15909466_key_1ServicePath/v1/my/listingsAuthToken _orderby=-ListPrice"
---
+++ sherwoodrealestate/ViewController.swift
@@ -0,0 +1,212 @@
+//
+//  ViewController.swift
+//  sherwoodrealestate
+//
+//  Created by Alex Beattie on 5/21/18.
+//  Copyright © 2018 Alex Beattie. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    var listings: [Listing.listingResults]?
+    var photos: [Listing.photoResults]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Listing.standardFields.fetchListing { (listings) -> () in
+            print(listings.D.Results)
+            self.listings = listings.D.Results
+            self.photos?.count
+            
+            self.collectionView?.reloadData()
+            
+            
+        }
+        navigationItem.title = "Sherwood Real Estate"
+        
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(HomeCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        
+    }
+    func showListingDetailController(_ listing: Listing.listingResults) {
+        let layout = UICollectionViewFlowLayout()
+        let listingDetailController = ListingDetailController(collectionViewLayout: layout)
+        
+        
+        listingDetailController.listing = listing
+        
+        
+        navigationController?.pushViewController(listingDetailController, animated: true)
+    }
+    // MARK: - Home CollectionViewController
+    
+    let homeCollectionView:UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return collectionView
+        
+    }()
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeCell
+        
+        cell.listing = listings?[indexPath.item]
+        return cell
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let count = listings?.count {
+            
+            
+            return count
+        }
+        return 0
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let listing = listings?[indexPath.item] {
+            showListingDetailController(listing)
+        }
+        
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView?.collectionViewLayout.invalidateLayout()
+    }
+}
+
+class HomeCell: UICollectionViewCell {
+    var listing: Listing.listingResults? {
+        didSet {
+//            setupThumbNailImage()
+
+            if let theAddress = listing?.StandardFields.UnparsedAddress {
+                nameLabel.text = theAddress
+            }
+            if let listPrice = listing?.StandardFields.ListPrice {
+                let nf = NumberFormatter()
+                nf.numberStyle = .decimal
+                let subTitleCost = "$\(nf.string(from: NSNumber(value:(UInt64(listPrice))))!)"
+                costLabel.text = subTitleCost
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Best New Apps"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let costLabel: UILabel = {
+        let label = UILabel()
+        label.text = "400"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.layer.masksToBounds = true
+        return iv
+    }()
+//    func setupThumbNailImage() {
+//        if let thumbnailImageUrl = listing?.photos?.first {
+//            imageView.loadImageUsingUrlString(urlString: thumbnailImageUrl)
+//        }
+//    }
+    func setupViews() {
+        backgroundColor = UIColor.clear
+        addSubview(nameLabel)
+        addSubview(costLabel)
+        addConstraintsWithFormat(format: "H:|-14-[v0]-14-|", views: nameLabel)
+        addConstraintsWithFormat(format: "V:[v0]-20-|", views: nameLabel)
+        
+        addConstraintsWithFormat(format: "H:|[v0]|", views: costLabel)
+        addConstraintsWithFormat(format: "V:[v0]-8-|", views: costLabel)
+        
+        
+//        addConstraintsWithFormat(format: "H:|[v0]|", views: imageView)
+//        addConstraintsWithFormat(format: "V:|[v0]|", views: imageView)
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views:viewsDictionary))
+    }
+    
+}
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setupViews() {
+    }
+}
+
